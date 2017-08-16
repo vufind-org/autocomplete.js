b849d6a4b7fd07a9dc98a603df422ae488ce4af1
Chris Hallberg <crhallberg@gmail.com>
2017-08-16 16:09:38 -0400

    Formatting with Prettier
    - Single quotes
    - Print width 110

--- autocomplete.js
+++ autocomplete.js
@@ -1,10 +1,10 @@
 /* https://github.com/vufind-org/autocomplete.js 1.0b2 */
-(function autocomplete( $ ) {
+(function autocomplete($) {
   var element = false,
     xhr = false;
 
   function Factory(_input, settings) {
-    return (function acClosure() {
+    return function acClosure() {
       var input = $(this),
         cache = {},
         options;
@@ -16,17 +16,17 @@
           left: position.left,
           minWidth: input.width()
         });
-      }
+      };
 
       var show = function show() {
         element.removeClass(options.hidingClass);
-      }
+      };
       var hide = function hide() {
         element.addClass(options.hidingClass);
-      }
+      };
 
       var _populate = function _populate(item, eventType) {
-        input.trigger('ac:select', [ item, eventType ]);
+        input.trigger('ac:select', [item, eventType]);
         var ret;
         if (options.callback) {
           ret = options.callback(item, input, eventType);
@@ -47,37 +47,33 @@
           input.focus();
           hide();
         }, 10);
-      }
+      };
 
       var _listToHTML = function _listToHTML(list, regex) {
         var shell = $('<div/>');
         for (var i = 0; i < list.length; i++) {
           if (typeof list[i] === 'string') {
-            list[i] = {value: list[i]};
+            list[i] = { value: list[i] };
           }
           var content = list[i].label || list[i].value;
           if (options.highlight) {
             content = content.replace(regex, '<b>$1</b>');
           }
-          var item = typeof list[i].href === 'undefined'
-            ? $('<div/>')
-            : $('<a/>').attr('href', list[i].href);
+          var item = typeof list[i].href === 'undefined' ? $('<div/>') : $('<a/>').attr('href', list[i].href);
           // list
-          item.data(list[i])
-              .addClass('ac-item')
-              .html(content);
+          item.data(list[i]).addClass('ac-item').html(content);
           if (typeof list[i].description !== 'undefined') {
-            item.append($('<small/>').html(
-              options.highlight
-                ? list[i].description.replace(regex, '<b>$1</b>')
-                : list[i].description
-            ));
+            item.append(
+              $('<small/>').html(
+                options.highlight ? list[i].description.replace(regex, '<b>$1</b>') : list[i].description
+              )
+            );
           }
           shell.append(item);
         }
-        input.trigger('ac:render', [ shell[0] ]);
+        input.trigger('ac:render', [shell[0]]);
         return shell;
-      }
+      };
       var _createList = function _createList(data) {
         // highlighting setup
         // escape term for regex - https://github.com/sindresorhus/escape-string-regexp/blob/master/index.js
@@ -93,10 +89,12 @@
               shell.append($('<hr/>', { class: 'ac-section-divider' }));
             }
             if (typeof data.groups[i].label !== 'undefined') {
-              shell.append($('<header>', {
-                class: 'ac-section-header',
-                html: data.groups[i].label
-              }));
+              shell.append(
+                $('<header>', {
+                  class: 'ac-section-header',
+                  html: data.groups[i].label
+                })
+              );
             }
             if (typeof data.groups[i].label !== 'undefined' && data.groups[i].items.length > 0) {
               shell.append(_listToHTML(data.groups[i].items, regex));
@@ -108,16 +106,17 @@
         element.html(shell);
         input.data('length', shell.find('.ac-item').length);
         element.find('.ac-item').mousedown(function acItemClick() {
-          _populate($(this).data(), {mouse: true});
+          _populate($(this).data(), { mouse: true });
         });
         _align();
-      }
+      };
 
       var _handleResults = function _handleResults(term, _data) {
         // Limit results
-        var data = typeof _data.groups === 'undefined'
-          ? _data.slice(0, Math.min(options.maxResults, _data.length))
-          : _data;
+        var data =
+          typeof _data.groups === 'undefined'
+            ? _data.slice(0, Math.min(options.maxResults, _data.length))
+            : _data;
         var cid = input.data('cacheId');
         cache[cid][term] = data;
         if (data.length === 0 || (typeof data.groups !== 'undefined' && data.groups.length === 0)) {
@@ -125,10 +124,11 @@
         } else {
           _createList(data);
         }
-      }
-      var _defaultStaticSort = function _defaultStaticSort(a, b) { // .bind(lcterm)
+      };
+      var _defaultStaticSort = function _defaultStaticSort(a, b) {
+        // .bind(lcterm)
         return a.match.indexOf(this) - b.match.indexOf(this);
-      }
+      };
       var _staticGroups = function _staticGroups(lcterm) {
         var matches = [];
         for (var i = 0; i < options.static.groups.length; i++) {
@@ -162,9 +162,11 @@
           }
         }
         return matches;
-      }
+      };
       var search = function search() {
-        if (xhr) { xhr.abort(); }
+        if (xhr) {
+          xhr.abort();
+        }
         if (input.val().length >= options.minLength) {
           element.html('<i class="ac-item loading">' + options.loadingString + '</i>');
           show();
@@ -173,13 +175,13 @@
           var term = input.val();
           // Check cache (only for handler-based setups)
           var cid = input.data('cacheId');
-          if (options.cache && typeof cache[cid][term] !== "undefined") {
+          if (options.cache && typeof cache[cid][term] !== 'undefined') {
             if (cache[cid][term].length === 0) {
               hide();
             } else {
               _createList(cache[cid][term]);
             }
-          // Check for static list
+            // Check for static list
           } else if (typeof options.static !== 'undefined') {
             var lcterm = term.toLowerCase();
             var matches;
@@ -196,7 +198,7 @@
               }
             }
             _handleResults(term, matches);
-          // Call handler
+            // Call handler
           } else {
             options.handler(input, function achandlerCallback(data) {
               _handleResults(term, data);
@@ -205,12 +207,10 @@
         } else {
           hide();
         }
-      }
+      };
 
       function preprocessStatic(_item) {
-        var item = typeof _item === 'string'
-          ? { value: _item }
-          : _item;
+        var item = typeof _item === 'string' ? { value: _item } : _item;
         item.match = (item.label || item.value).toLowerCase();
         return item;
       }
@@ -257,26 +257,26 @@
             return;
           }
           switch (event.which) {
-          case 9:    // tab
-          case 13:   // enter
-          case 16:   // shift
-          case 20:   // caps lock
-          case 27:   // esc
-          case 33:   // page up
-          case 34:   // page down
-          case 35:   // end
-          case 36:   // home
-          case 37:   // arrows
-          case 38:
-          case 39:
-          case 40:
-          case 45:   // insert
-          case 144:  // num lock
-          case 145:  // scroll lock
-          case 19:   // pause/break
-            return;
-          default:
-            search();
+            case 9: // tab
+            case 13: // enter
+            case 16: // shift
+            case 20: // caps lock
+            case 27: // esc
+            case 33: // page up
+            case 34: // page down
+            case 35: // end
+            case 36: // home
+            case 37: // arrows
+            case 38:
+            case 39:
+            case 40:
+            case 45: // insert
+            case 144: // num lock
+            case 145: // scroll lock
+            case 19: // pause/break
+              return;
+            default:
+              search();
           }
         });
         input.keydown(function acinputKeydown(event) {
@@ -287,58 +287,58 @@
           var position = $(this).data('selected');
           switch (event.which) {
             // arrow keys through items
-          case 38: // up key
-            event.preventDefault();
-            element.find('.ac-item.selected').removeClass('selected');
-            if (position > -1) {
-              if (position-- > 0) {
-                element.find('.ac-item:eq(' + position + ')').addClass('selected');
-              }
-              $(this).data('selected', position);
-            }
-            break;
-          case 40: // down key
-            event.preventDefault();
-            if (element.hasClass(options.hidingClass)) {
-              search();
-            } else if (position < input.data('length') - 1) {
-              position++;
-              element.find('.ac-item.selected').removeClass('selected');
-              element.find('.ac-item:eq(' + position + ')').addClass('selected');
-              $(this).data('selected', position);
-            }
-            break;
-            // enter to nav or populate
-          case 9:
-          case 13:
-            var selected = element.find('.ac-item.selected');
-            if (selected.length > 0) {
+            case 38: // up key
               event.preventDefault();
-              if (event.which === 13 && selected.attr('href') && options.callback === 'undefined') {
-                return window.location.assign(selected.attr('href'));
-              } else {
-                _populate(selected.data(), $(this), {key: true});
+              element.find('.ac-item.selected').removeClass('selected');
+              if (position > -1) {
+                if (position-- > 0) {
+                  element.find('.ac-item:eq(' + position + ')').addClass('selected');
+                }
+                $(this).data('selected', position);
               }
-            }
-            break;
+              break;
+            case 40: // down key
+              event.preventDefault();
+              if (element.hasClass(options.hidingClass)) {
+                search();
+              } else if (position < input.data('length') - 1) {
+                position++;
+                element.find('.ac-item.selected').removeClass('selected');
+                element.find('.ac-item:eq(' + position + ')').addClass('selected');
+                $(this).data('selected', position);
+              }
+              break;
+            // enter to nav or populate
+            case 9:
+            case 13:
+              var selected = element.find('.ac-item.selected');
+              if (selected.length > 0) {
+                event.preventDefault();
+                if (event.which === 13 && selected.attr('href') && options.callback === 'undefined') {
+                  return window.location.assign(selected.attr('href'));
+                } else {
+                  _populate(selected.data(), $(this), { key: true });
+                }
+              }
+              break;
             // hide on escape
-          case 27:
-            hide();
-            $(this).data('selected', -1);
-            break;
+            case 27:
+              hide();
+              $(this).data('selected', -1);
+              break;
           }
         });
 
-        window.addEventListener("resize", hide, false);
-      }
+        window.addEventListener('resize', hide, false);
+      };
 
-      if (typeof settings === "string") {
-        if (settings === "show") {
+      if (typeof settings === 'string') {
+        if (settings === 'show') {
           show();
           _align();
-        } else if (settings === "hide") {
+        } else if (settings === 'hide') {
           hide();
-        } else if (options.cache && settings === "clear cache") {
+        } else if (options.cache && settings === 'clear cache') {
           var cid = parseInt(input.data('cacheId'), 10);
           cache[cid] = {};
         }
@@ -361,12 +361,12 @@
             settings.static = settings.static.map(preprocessStatic);
           }
         }
-        options = $.extend( {}, $.fn.autocomplete.defaults, settings );
+        options = $.extend({}, $.fn.autocomplete.defaults, settings);
         _setup();
       }
 
       return input;
-    }.bind(_input))();
+    }.bind(_input)();
   }
 
   $.fn.autocomplete = function acJQuery(settings) {
@@ -386,12 +386,14 @@
 
   var timer = false;
   $.fn.autocomplete.ajax = function acAjax(ops) {
-    if (timer) { clearTimeout(timer); }
-    if (xhr) { xhr.abort(); }
-    timer = setTimeout(
-      function acajaxDelay() { xhr = $.ajax(ops); },
-      200
-    );
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (xhr) {
+      xhr.abort();
+    }
+    timer = setTimeout(function acajaxDelay() {
+      xhr = $.ajax(ops);
+    }, 200);
   };
-
-}( jQuery ));
+})(jQuery);
